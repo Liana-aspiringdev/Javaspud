02d568a8b80d5677e7af990ed811c41ee4ad8449
Liana-aspiringdev <Rodriguez.Liana@student.greenriver.edu>
2025-11-26 11:51:38 -0800

    Added toggle for scheme and changing font size

--- potato.js
+++ potato.js
@@ -1,1 +1,16 @@
 // Add your javascript in this file
+
+function toggleScheme() {
+    console.log("itsbeautiful")
+    document.body.classList.toggle("makeover")
+}
+
+function toggleSize() {
+    console.log("let'smakeitbigger!")
+    document.body.classList.toggle("toggleSize")
+}
+
+function facts() {
+    console.log("Afact!")
+    document.getElementsByID("faq").innerHTML = "In 1995 the potato became the first vegetable to grow in space"
+}
